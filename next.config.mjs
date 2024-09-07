74775a0e69f6be45fb1404e55762d662254fec0d
MiTange <tmrevo210@yahoo.co.jp>
2024-09-07 22:22:18 +0900

    CORSのエラー解消に向けて

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://next-market-5yrxqxme4-m-tange-ppps-projects.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "Content-Type"
+          },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
